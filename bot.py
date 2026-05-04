dbe89d40040a89b3a28b75f87a347a60a056028c
jailton-silvasp <131924910+jailton-silvasp@users.noreply.github.com>
2026-05-04 18:17:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -181,6 +181,12 @@ async def on_message(message):
 
         except:
             await message.channel.send("❌ Use: !vs 2.5")
+            
+        if message.content == "!ranking":
+    await enviar_ranking()
+
+if message.content == "!meta":
+    await verificar_meta()
 
 
 # =========================================
