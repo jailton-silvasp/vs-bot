9ed5768835663b2591fc7c5dd71228ff021c6c22
jailton-silvasp <131924910+jailton-silvasp@users.noreply.github.com>
2026-05-04 11:22:41 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,48 @@
+import discord
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import os
+import json
+
+# ===== DISCORD =====
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# ===== GOOGLE =====
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client_gspread = gspread.authorize(creds)
+
+sheet = client_gspread.open("Controle VS").sheet1
+
+@client.event
+async def on_ready():
+    print(f'Logado como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content.startswith("!vs"):
+        try:
+            valor = float(message.content.split(" ")[1])
+            usuario = str(message.author)
+            data = datetime.now().strftime("%d/%m/%Y")
+
+            sheet.append_row([data, usuario, valor])
+
+            await message.channel.send(f"✅ VS registrado: {valor}M")
+        except:
+            await message.channel.send("❌ Use: !vs 2.5")
+
+client.run(TOKEN)
