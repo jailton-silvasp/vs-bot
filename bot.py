e354917d21bc99220b439a3bd184a1330b8ca804
jailton-silvasp <131924910+jailton-silvasp@users.noreply.github.com>
2026-05-06 11:26:38 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import os
 import discord
 from discord.ext import commands
 import requests
 from datetime import datetime
 
-TOKEN = "MTUwMDE4MzI0MTQ5MDA0MjkzMA.GJHFeD.NMupUx8sH25KjYJeO5C-Xeg_I_5wMlxS7eDVrM"
-API_URL = "https://api-svs-production.up.railway.app"
+TOKEN = os.getenv("TOKEN")
+API_URL = os.getenv("API_URL")
 
 intents = discord.Intents.default()
 intents.message_content = True
